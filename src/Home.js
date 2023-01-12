ba839a8df8b1b1b5d3e57ffaf9912ee84b37409c
Adrian Petrykiw <apetry2@illinois.edu>
2023-01-11 19:40:54 -0600

    updated ui

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import { useDynamicContext, DynamicWidget } from "@dynamic-labs/sdk-react";
 import { useState, useEffect } from "react";
 import "./Home.css";
-import { Box } from '@chakra-ui/react'
+import { Box, Select, Input } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 function Home() {
   // const {
@@ -21,23 +22,46 @@ function Home() {
   //   }
   // }, [user.walletPublicKey]);
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
 
   return (
-    <div className="Home-body">
-    {/* <DynamicWidget /> */}
-      <div className="Home-header">
-        <p>Home</p>
-      </div>
-      <div className="Gallery"></div>
-      <div className="Gallery-header">
-        <p>Choose your NFT</p>
-      </div>
-      <div>
-      </div>
-      <div className="form-widget">
-
-      </div>
+    <div className="Homer">
+      <Box w="100%" bg="#ED1C24" className="Home-header">
+        <p className="p">WARETA</p>
+        {/* <DynamicWidget className="logout-button"/> */}
+      </Box>
+      <Box className="Home-body">
+        <Box className="form-widget">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p className="p2">WARETA</p>
+{/* 
+            <div>
+              <div className="Gallery"></div>
+              <div className="Gallery-header">
+                <p>Choose your NFT</p>
+              </div>
+            </div> */}
+            <div>
+              <Select placeholder="Select option">
+                <option value="option1">Deep Fry</option>
+                <option value="option2">Pixelate</option>
+              </Select>{" "}
+            </div>
+            <div>
+              <Input type="submit" />
+            </div>
+          </form>
+        </Box>
+      </Box>
     </div>
   );
 }
