9effc7675d6640c1ada08d729139c83900fb31e2
Antony-SS <jns5@illinois.edu>
2023-01-23 20:36:12 -0600

    adding text response from open ai

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,4 +1,4 @@
-import { Image, GridItem } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 
 
---
+++ src/utils/get_ai.js
@@ -0,0 +1,23 @@
+
+const api_key = process.env.CHAT_GPT;
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.CHAT_GPT,
+});
+
+
+const openai = new OpenAIApi(configuration);
+
+async function get_text_response(meme_description) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Write me a meme caption for a meme with the following desciption: " + meme_description,
+        temperature: 0,
+        max_tokens: 7,
+      });
+}
+
+
+
+
