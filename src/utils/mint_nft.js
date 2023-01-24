6486842f92890be1d6e99b31ae59171c6fb1678b
Antony-SS <jns5@illinois.edu>
2023-01-24 00:26:35 -0600

    continuing

--- src/utils/mint_nft.js
+++ src/utils/mint_nft.js
@@ -1,15 +1,14 @@
-import { Metaplex } from "@metaplex-foundation/js";
+import { Metaplex, bundlrStorage } from "@metaplex-foundation/js";
 import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
 
 SECRET_KEY = process.env.wareta_wallet_sk;
 const wallet = Keypair.fromSecretKey(SECRET_KEY);
 
 const connection = new Connection(clusterApiUrl("mainnet-beta"));
-const metaplex = new Metaplex(connection).use(supabase).use(wallet); // this might be a problem
+const metaplex = new Metaplex(connection).use(bundlrStorage()).use(wallet); // this might be a problem
 
 import { BigNumber } from "ethers";
 import { supabase } from "../supabaseClient";
-
 
 
 export default async function mint_nft(title, image_url, orignal_nft_title, minter_wallet_address) {
@@ -23,18 +22,14 @@ export default async function mint_nft(title, image_url, orignal_nft_title, mint
     const { nft } = await metaplex.nfts().create({
         uri: uri,
         name: "My NFT",
-        sellerFeeBasisPoints: 0, // Represents 0.00%.
+        sellerFeeBasisPoints: 0,
         updateAuthority: metaplex.identity(),
         tokenOwner: minter_wallet_address,
         maxSupply: BigNumber.from(1)
     });
 
-    
+    console.log("MINTED NFT: " + nft);
 
-
-
-
-
-
+    return nft;
 
 }
