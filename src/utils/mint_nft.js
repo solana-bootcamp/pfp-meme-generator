391166ea6ccd601d147643572b6d7d88ea8c71eb
Antony-SS <jns5@illinois.edu>
2023-01-24 00:08:33 -0600

    update

--- src/utils/mint_nft.js
+++ src/utils/mint_nft.js
@@ -1,30 +1,32 @@
 import { Metaplex } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
+
+SECRET_KEY = process.env.wareta_wallet_sk;
+const wallet = Keypair.fromSecretKey(SECRET_KEY);
 
 const connection = new Connection(clusterApiUrl("mainnet-beta"));
-const metaplex = new Metaplex(connection).use(supabase); // this might be a problem
+const metaplex = new Metaplex(connection).use(supabase).use(wallet); // this might be a problem
 
 import { BigNumber } from "ethers";
-
 import { supabase } from "../supabaseClient";
 
+
+
 export default async function mint_nft(title, image_url, orignal_nft_title, minter_wallet_address) {
-    // first we upload metadata to supabase
 
     const { uri } = await metaplex.nfts().uploadMetadata({
         name: title,
         description: "Memed the NFT: " + orignal_nft_title,
         image: image_url,
-        tokenOwner: minter_wallet_address,
-        maxSupply: BigNumber.from(1),
-
     });
 
     const { nft } = await metaplex.nfts().create({
-        uri: "",
+        uri: uri,
         name: "My NFT",
-        sellerFeeBasisPoints: 500, // Represents 5.00%.
-        updateAuthority: 
+        // sellerFeeBasisPoints: 500, // Represents 5.00%.
+        updateAuthority: wallet,
+        tokenOwner: minter_wallet_address,
+        maxSupply: BigNumber.from(1)
     });
 
 
