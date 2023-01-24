3da4eb6d4db281be9e4d5ce550d8ca6d0de17aee
Antony-SS <jns5@illinois.edu>
2023-01-23 22:30:34 -0600

    putting in chat gpt

--- src/utils/callChatGPT.js
+++ src/utils/callChatGPT.js
@@ -1,8 +1,21 @@
 import { supabase } from "../supabaseClient";
 import { useState, useEffect } from "react";
 
+const api_key = process.env.CHAT_GPT;
+const { Configuration, OpenAIApi } = require("openai");
 
-//call chat GPT to get meme text
-export default async function callChatGPT({ prompt }) {
-      return "Some prompt"
+const configuration = new Configuration({
+  apiKey: process.env.CHAT_GPT,
+});
+
+
+const openai = new OpenAIApi(configuration);
+
+async function callChatGPT(meme_description) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Write me a meme caption for a meme with the following desciption: " + meme_description,
+        temperature: 0,
+        max_tokens: 7,
+      });
 }
